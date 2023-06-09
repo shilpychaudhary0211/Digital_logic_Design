f38a9796482041e419e2a2edd499d81017a6565a
shilpy chaudhary <133845445+shilpychaudhary0211@users.noreply.github.com>
2023-06-09 07:21:36 +0530

    Update 2.Full_adder code.vhdl

--- 2.Full_adder code.vhdl
+++ 2.Full_adder code.vhdl
@@ -1,1 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
+entity full_adder is
+  port(a,b,c: in std_logic;
+       sum,carry:out std_logic);
+  end full_adder;
+  
+  architecture data of full_adder is begin 
+    sum <= a xor b xor c;
+    carry<= ((a and b) or (b and c) or (a and c));
+  end data;
