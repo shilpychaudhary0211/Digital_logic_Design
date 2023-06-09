1998bba79877321b111b3c7f880cfd34e15f2a5d
shilpy chaudhary <133845445+shilpychaudhary0211@users.noreply.github.com>
2023-06-09 07:23:21 +0530

    Update 3.Mux 2*1 code.vhdl

--- 3.Mux 2*1 code.vhdl
+++ 3.Mux 2*1 code.vhdl
@@ -1,1 +1,11 @@
-
+library ieee;
+use ieee.std_logic_1164.all;
+entity mux 2*1 is
+  port(i0,i1,s: in std_logic;
+       y: out std_logic);
+  end;
+  architecture mydesign of mux 2*1 is 
+    begin 
+      y<= (i1 and s) or ((not s) and i0);
+    end mydesign;
+    
