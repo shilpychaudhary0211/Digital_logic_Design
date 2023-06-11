e43337abbef23a714abd17b160ecd2932be8adb4
Shilpy Chaudhary <133845445+shilpychaudhary0211@users.noreply.github.com>
2023-06-11 19:18:08 +0530

    Update 4.SR latch code.vhdl

--- 4.SR latch code.vhdl
+++ 4.SR latch code.vhdl
@@ -1,13 +1,17 @@
-library ieee; 
-use ieee.std_logic_1164.all;
-  
-entity srl is 
-   port(r,s:in bit; q,qbar:buffer bit); 
-end srl;  
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
-architecture virat of srl is 
-   signal s1,r1:bit; 
-begin 
-   q<= s nand qbar; 
-   qbar<= r nand q; 
-end virat; 
+entity S_R_latch_top is
+    Port ( S : in    STD_LOGIC;
+           R : in    STD_LOGIC;
+           Q : out   STD_LOGIC);
+end S_R_latch_top;
+
+architecture Behavioral of S_R_latch_top is
+signal notQ : STD_LOGIC;
+begin
+
+Q    <= R nor notQ;
+notQ <= S nor Q;
+
+end Behavioral;
