1d60796cce05df8f64ceef66e3546a77341cc4d5
Shilpy Chaudhary <133845445+shilpychaudhary0211@users.noreply.github.com>
2023-06-11 19:17:02 +0530

    Update 4.SR latch code.vhdl

--- 4.SR latch code.vhdl
+++ 4.SR latch code.vhdl
@@ -1,1 +1,13 @@
+library ieee; 
+use ieee.std_logic_1164.all;
+  
+entity srl is 
+   port(r,s:in bit; q,qbar:buffer bit); 
+end srl;  
 
+architecture virat of srl is 
+   signal s1,r1:bit; 
+begin 
+   q<= s nand qbar; 
+   qbar<= r nand q; 
+end virat; 
